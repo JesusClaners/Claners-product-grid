8d7c63e8eebff94683459da4498893735ac7f80e
JesusClaners <91222322+JesusClaners@users.noreply.github.com>
2021-10-19 13:37:41 -0700

    Falta crear mas variables en css

---
+++ Componentes de React/App.js
@@ -0,0 +1,20 @@
+import './Claners-grid.css';
+import Itemoffer from './Item-offer';
+
+function App() {
+  return (
+
+      <div className="grid-Jesus">
+        <div className="grid-container-offers ">
+          <Itemoffer className="offer-item"/>
+          <Itemoffer className="offer-item"/>
+          <Itemoffer className="offer-item"/>
+          <Itemoffer className="offer-item"/>
+          <Itemoffer className="offer-item"/>
+          <Itemoffer className="offer-item"/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
