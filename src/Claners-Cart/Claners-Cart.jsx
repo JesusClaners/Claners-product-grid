120d6b9926090c5305450021d0ceb53e5b4eff1c
JesusClaners <91222322+JesusClaners@users.noreply.github.com>
2021-10-22 14:18:58 -0700

    Crear menu y componer la estructura
    Estructura a base de flexbox

---
+++ src/Claners-Cart/Claners-Cart.jsx
@@ -0,0 +1,30 @@
+import "/Claners-Cart.css"
+
+
+const ClanersCart =()=>{
+
+    return(
+
+        <div className="Claners-cart">
+           
+            <div className="cart-container">
+
+                <div className="cart">
+
+                    <div className="cart-items-list">
+
+                        <div className="cart-items"></div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default ClanersCart
